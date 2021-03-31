236099007c85d557389a7eee35927bae47718524
mcasiano1 <yssa_casiano@yahoo.com>
2021-03-30 21:26:04 -0500

    updated file push

---
+++ Resources/create_tables.sql
@@ -0,0 +1,34 @@
+-- Table: public.year_2018 & year_2019
+
+-- DROP TABLE public.year_2018 & year_2019;
+
+CREATE TABLE public.year_2018
+(
+    "Overall rank" integer,
+    "Country or region" name,
+    "Score" numeric,
+    year_column integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.year_2018
+
+    OWNER to postgres;
+
+CREATE TABLE public.year_2019
+(
+    "Overall rank" integer,
+    "Country or region" name,
+    "Score" numeric,
+    year_column integer
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.year_2019
+    OWNER to postgres;
